3c749ff380ffdde910fe92dd13a6b0507fe140d8
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-03-05 00:34:10 +0530

    Create Build Array from Permutation.java

---
+++ L12ArrayALists/Build Array from Permutation.java
@@ -0,0 +1,12 @@
+Question : https://leetcode.com/problems/build-array-from-permutation/description/
+Code :
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[n];
+        for(int i = 0;i<n;i++)
+            arr[i] = nums[nums[i]];
+            return arr;
+        
+    }
+}
