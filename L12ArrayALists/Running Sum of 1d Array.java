f715c97d86fe800618b04ef6e4c87ca98635034a
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-03-06 12:44:59 +0530

    Create Running Sum of 1d Array.java

---
+++ L12ArrayALists/Running Sum of 1d Array.java
@@ -0,0 +1,12 @@
+Question : https://leetcode.com/problems/running-sum-of-1d-array/description/
+Code : 
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        for(int i=1;i<n;i++){
+            nums[i] = nums[i-1]+nums[i];
+           
+        }
+         return nums;
+    }
+}
