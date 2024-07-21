1bc28e0cdbd5932ec9011086cb612882012589cc
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-07-21 21:11:49 +0530

    Create Find the Duplicate Number.java

---
+++ Sorting/Find the Duplicate Number.java
@@ -0,0 +1,14 @@
+Question : https://leetcode.com/problems/find-the-duplicate-number/description/
+Code :
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+            int ind = Math.abs(nums[i]);
+            if(nums[ind] < 0) {
+                return ind;
+            }
+            nums[ind] = -nums[ind];
+        }
+        return -1;
+    }
+}
